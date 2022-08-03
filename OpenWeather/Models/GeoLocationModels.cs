1405a5b0d687a97db837cb1ada86812c08519745
Andrew McCullough <a.mccullough@outlook.com>
2022-08-03 14:52:22 -0400

    Add basic geo location models for open weather map api responses.

---
+++ OpenWeather/Models/GeoLocationModels.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenWeather.Models
+{
+    public record Coordinate
+    {
+        [JsonPropertyName("lat")]
+        public double Latitude { get; set; }
+
+        [JsonPropertyName("lon")]
+        public double Longitude { get; set; }
+    }
+
+    public abstract record LocationResult : Coordinate
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; } = string.Empty;
+    }
+
+    public record PostalCodeResult : LocationResult
+    {
+        [JsonPropertyName("zip")]
+        public string PostCode { get; set; } = string.Empty;
+    }
+
+    public record LocationNameResult : LocationResult
+    {
+
+        [JsonPropertyName("local_names")]
+        public Dictionary<string, string> LocalNames { get; set; } = new();
+
+        [JsonPropertyName("state")]
+        public string State { get; set; } = string.Empty;
+    }
+}
